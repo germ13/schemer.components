a16cd09d4e6c967e77a5077e1c36c71ebda1eec2
juan serrano <germgerm@gmail.com>
2021-01-01 23:46:54 -0800

    Create basic react scaffolding

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render(){
+        return(
+            <div className="App">
+                <h1>I live!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
